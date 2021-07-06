6d55c963100c9a84558190180bf23c249ab91c14
joyling93 <963034741@qq.com>
2021-07-06 11:43:21 +0800

    1.0

--- slingshot.R
+++ slingshot.R
@@ -1,18 +1,27 @@
 library(Seurat)
-library(slingshot)
-library(Matrix)
-sce <- readRDS()
+library(slingshot,quietly = T)
+#library(Matrix)
+#slingPseudotime
+#slingCurveWeights
+rds.path <- '/PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/demo_seurat.rds'
+sce <- readRDS(rds.path)
 
 cl<-sce$seurat_clusters
 rd<-Embeddings(sce, "umap")
 
-sds <- slingshot(rd, clusterLabels = cl)
+sds <- slingshot(rd, clusterLabels = cl, reducedDim = 'umap',start.clus =5,end.clus =8)
 
-png('line.png')
-plot(SlingshotDataSet(sds), col = brewer.pal(9,"Set1")[cl], asp = 1, pch = 16)
+ident.colors <- (scales::hue_pal())(n = length(x = levels(x = sce)))
+names(x = ident.colors) <- levels(x = sce)
+cell.colors <- ident.colors[Idents(object = sce)]
+names(x = cell.colors) <- colnames(x = sce)
+
+png('curv.png')
+plot(Embeddings(sce, reduction = 'umap'), col = cell.colors, asp = 1, pch = 16)
 lines(SlingshotDataSet(sds), lwd=2, col='black')
 dev.off()
 
-png('curv.png')
+png('line.png')
+plot(Embeddings(sce, reduction = 'umap'), col = cell.colors, asp = 1, pch = 16)
 lines(SlingshotDataSet(sds), lwd=2, col='black',type='l')
 dev.off()
